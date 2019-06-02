25f402969bfb39c8834204aa6059d72e97308510
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-06-02 23:15:37 +0430

    initial commit

---
+++ run.py
@@ -0,0 +1,13 @@
+
+"""
+.. sectionauthor:: Ali ArjomandBigdeli  <https://github.com/aliarjomandbigdeli>
+.. since:: 6/2/2019
+"""
+
+
+def main():
+    print("Hello Ali")
+
+
+if __name__ == '__main__':
+    main()
