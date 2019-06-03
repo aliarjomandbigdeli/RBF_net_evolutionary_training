8c24edaf465d53eae2e869a92588dbe5f0f9c2b3
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-06-03 11:23:47 +0430

    complete mutation method

--- rbf_es.py
+++ rbf_es.py
@@ -1,6 +1,7 @@
 from sklearn.datasets.samples_generator import make_blobs
 from numpy import linalg as la
 import random
+import math
 
 """
 .. sectionauthor:: Ali ArjomandBigdeli  <https://github.com/aliarjomandbigdeli>
@@ -14,6 +15,7 @@ class ES:
         self._population_size = 10000
         self._chromosome_max_size = 10  # in this version length of chromosomes aren't constant
         self._gene_fields_number = 3  # x,y,r
+        self._tau = 1 / self._population_size ** 0.5
         self._children = []
         self._best_chromosome = []
 
@@ -26,7 +28,14 @@ class ES:
             self._population.append(chromosome)
 
     def mutation(self):
-        print("mutation")
+        for chromosome in self._population:
+            # mutate σ at first
+            sigma = chromosome[0]
+            sigma = sigma * math.exp(self._tau * random.normalvariate(mu=0, sigma=1))
+            chromosome[0] = sigma
+            # mutate other genes
+            for i in range(1, len(chromosome)):
+                chromosome[i] = chromosome[i] + sigma * random.normalvariate(mu=0, sigma=1)
 
     def crossover(self):
         print("parent selection")
