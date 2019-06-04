b5ff341762c8a0a3ceb57a8eb7d0b05da10d2686
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-06-04 21:37:52 +0430

    modify algorithm by adding mutated_population field

--- rbf_es.py
+++ rbf_es.py
@@ -126,6 +126,9 @@ class RBFRegression:
         self._base_fields_number = 2  # x,r (dimension + 1(for radius))
         self._tau = 0.5 / ((self._base_fields_number * self._chromosome_max_bases) ** 0.5)
         self._children = []
+        self._best_chromosome = []
+        self._best_fitness_list = []
+        self._avg_fitness_list = []
 
     def data(self, d=None):
         """getter and setter of data"""
@@ -164,19 +167,63 @@ class RBFRegression:
             self._population.append(chromosome)
 
     def mutation(self):
+        # for chromosome in self._population:
+        #     # mutate σ at first
+        #     sigma = chromosome[0]
+        #     sigma = sigma * math.exp(self._tau * random.normalvariate(mu=0, sigma=1))
+        #     # print(f'past sigma: {chromosome[0]}, new sigma: {sigma}')
+        #     chromosome[0] = sigma
+        #     # mutate other genes
+        #     for i in range(1, len(chromosome)):
+        #         chromosome[i] = chromosome[i] + sigma * random.normalvariate(mu=0, sigma=1)
+
+        self._mutated_population = []
         for chromosome in self._population:
+            mutated_chromosome = []
             # mutate σ at first
             sigma = chromosome[0]
             sigma = sigma * math.exp(self._tau * random.normalvariate(mu=0, sigma=1))
             # print(f'past sigma: {chromosome[0]}, new sigma: {sigma}')
-            chromosome[0] = sigma
+            mutated_chromosome.append(sigma)
             # mutate other genes
             for i in range(1, len(chromosome)):
-                chromosome[i] = chromosome[i] + sigma * random.normalvariate(mu=0, sigma=1)
+                mutated_chromosome.append(chromosome[i] + sigma * random.normalvariate(mu=0, sigma=1))
+            self._mutated_population.append(mutated_chromosome)
+
+        # for i in range(self._population_size):
+        #     # mutate σ at first
+        #     sigma = self._population[i][0]
+        #     sigma = sigma * math.exp(self._tau * random.normalvariate(mu=0, sigma=1))
+        #     # print(f'past sigma: {chromosome[0]}, new sigma: {sigma}')
+        #     self._mutated_population[i][0] = sigma
+        #     # mutate other genes
+        #     for j in range(1, len(self._population[i])):
+        #         self._mutated_population[i][j] = self._population[i][j] + sigma * random.normalvariate(mu=0, sigma=1)
 
     def crossover(self):
-        parent1 = self._population[random.randint(0, self._population_size - 1)]
-        parent2 = self._population[random.randint(0, self._population_size - 1)]
+        # parent1 = self._population[random.randint(0, self._population_size - 1)]
+        # parent2 = self._population[random.randint(0, self._population_size - 1)]
+        # shorter_parent = parent1
+        # longer_parent = parent2
+        # if len(longer_parent) < len(shorter_parent):
+        #     shorter_parent = parent2
+        #     longer_parent = parent1
+        #
+        # self._children = []
+        # for i in range(7 * self._population_size):
+        #     child = []
+        #
+        #     for j in range(len(shorter_parent)):
+        #         child.append((shorter_parent[j] + longer_parent[j]) / 2)
+        #     # if self.fitness(longer_parent) > self.fitness(shorter_parent):
+        #     if i % 2 == 0:
+        #         for j in range(len(shorter_parent), len(longer_parent)):
+        #             child.append(longer_parent[j])
+        #
+        #     self._children.append(child)
+
+        parent1 = self._mutated_population[random.randint(0, self._population_size - 1)]
+        parent2 = self._mutated_population[random.randint(0, self._population_size - 1)]
         shorter_parent = parent1
         longer_parent = parent2
         if len(longer_parent) < len(shorter_parent):
@@ -187,62 +234,36 @@ class RBFRegression:
         for i in range(7 * self._population_size):
             child = []
 
-            for j in range(len(shorter_parent)):
+            child.append(shorter_parent[0])
+            for j in range(1, len(shorter_parent)):
                 child.append((shorter_parent[j] + longer_parent[j]) / 2)
             # if self.fitness(longer_parent) > self.fitness(shorter_parent):
-            if i % 2 == 0:
-                for j in range(len(shorter_parent), len(longer_parent)):
-                    child.append(longer_parent[j])
+            # if i % 2 == 0:
+            for j in range(len(shorter_parent), len(longer_parent)):
+                child.append(longer_parent[j])
 
             self._children.append(child)
-
-    # def make_wheel(self, population):
-    #     wheel = []
-    #     total = sum(self.fitness(p) for p in population)
-    #     top = 0
-    #     for p in population:
-    #         f = self.fitness(p) / total
-    #         wheel.append((top, top + f, p))
-    #         top += f
-    #     return wheel
-    #
-    # def bin_search(self, wheel, num):
-    #     mid = len(wheel) // 2
-    #     low, high, answer = wheel[mid]
-    #     if low <= num <= high:
-    #         return answer
-    #     elif low > num:
-    #         return self.bin_search(wheel[mid + 1:], num)
-    #     else:
-    #         return self.bin_search(wheel[:mid], num)
-    #
-    # def select(self, wheel, n_select):
-    #     """ this method selects chromosome based on SUS(Stochastic Universal Sampling)"""
-    #     step_size = 1.0 / n_select
-    #     new_generation = []
-    #     r = random.random()
-    #     new_generation.append(self.bin_search(wheel, r))
-    #     while len(new_generation) < n_select:
-    #         r += step_size
-    #         if r > 1:
-    #             r %= 1
-    #         new_generation.append(self.bin_search(wheel, r))
-    #     return new_generation
-    #
-    # def survivors_selection(self):
-    #     wheel = self.make_wheel(self._children)
-    #     self._population = self.select(wheel, self._population_size)
 
     def select_best(self, chromosome_list):
         bst = chromosome_list[0]
         for i in chromosome_list:
             # fit = self.fitness(i)
             fit = self.fitness(bst)
-            print(f'fitness: {fit}')
+            # print(f'fitness: {fit}')
             # if self.fitness(bst) < self.fitness(i):
             if fit < self.fitness(i):
                 bst = i
         return bst
+
+    def return_best_avg_fit(self, chromosome_list):
+        s = 0
+        bst = chromosome_list[0]
+        for i in chromosome_list:
+            fit = self.fitness(i)
+            s += fit
+            if self.fitness(bst) < fit:
+                bst = i
+        return self.fitness(bst), s / len(chromosome_list)
 
     def survivors_selection(self):
         """ this method works based on q-tournament """
@@ -251,10 +272,10 @@ class RBFRegression:
         for i in range(self._population_size):
             batch = []
             for j in range(q):
-                r = random.randint(0, 8 * self._population_size - 1)
+                r = random.randint(0, (7 + 1) * self._population_size - 1)
                 if r < self._population_size:
                     batch.append(self._population[r])
-                elif r >= self._population_size:
+                else:
                     batch.append(self._children[r - self._population_size])
             new_population.append(self.select_best(batch))
 
@@ -269,8 +290,12 @@ class RBFRegression:
             self.crossover()
             self.survivors_selection()
             print(f'iter {i}')
+            bst, avg = self.return_best_avg_fit(self._population)
+            self._best_fitness_list.append(bst)
+            self._avg_fitness_list.append(avg)
 
-        self.fitness(self.select_best(self._population))
+        self._best_chromosome = self.select_best(self._population)
+        print(self.fitness(self._best_chromosome))  # just for updating y
 
     def fitness(self, chromosome):
         self.calculate_matrices(chromosome)
@@ -297,155 +322,154 @@ class RBFRegression:
         self._w = la.inv(g.transpose().dot(g) + lam * np.identity(len(centers))).dot(g.transpose()).dot(self._y_star)
         self._y = g.dot(self._w)
 
-
-class RBFClassifier:
-    def __init__(self):
-        self._data = []
-        self._dimension = 2  # number of features
-        self._y_star = []
-        self._y = []
-        self._g = []
-        self._w = []  # weight matrix
-
-        self._min_range = -10
-        self._max_range = 10
-
-        self._population = []
-        self._population_size = 1000
-        self._chromosome_max_size = 10  # in this version length of chromosomes aren't constant
-        self._gene_fields_number = 3  # x,y,r
-        self._tau = 1 / self._gene_fields_number ** 0.5
-        self._children = []
-        self._best_chromosome = []
-
-    def data(self, d=None):
-        """getter and setter of data"""
-        if d:
-            self._data = d
-        return self._data
-
-    def y(self):
-        """:returns predicted vector"""
-        return self._y
-
-    def y_star(self):
-        return self._y_star
-
-    def create_random_dataset(self, num_of_data, dimension, cluster_number):
-        """create random dataset by normal distribution"""
-        x, y = make_blobs(n_samples=num_of_data, centers=cluster_number, n_features=dimension)
-        self._dimension = dimension
-        self._data = x
-        self._y_star = y
-
-        self._max_range = max(self._data[:, 0])
-        self._min_range = min(self._data[:, 0])
-
-    def initialize_population(self, max_range, min_range):
-        # chromosome representation : <σ,x1,y1,r1,x2,y2,r2,...>
-        for i in range(self._population_size):
-            chromosome = [(max_range - min_range) * 0.2]  # add σ to chromosome
-            for j in range(self._gene_fields_number * random.randint(2, self._chromosome_max_size)):
-                chromosome.append(random.random() * (max_range - min_range) + min_range)
-            self._population.append(chromosome)
-
-    def mutation(self):
-        for chromosome in self._population:
-            # mutate σ at first
-            sigma = chromosome[0]
-            sigma = sigma * math.exp(self._tau * random.normalvariate(mu=0, sigma=1))
-            chromosome[0] = sigma
-            # mutate other genes
-            for i in range(1, len(chromosome)):
-                chromosome[i] = chromosome[i] + sigma * random.normalvariate(mu=0, sigma=1)
-
-    def crossover(self):
-        parent1 = self._population[random.randint(0, self._population_size)]
-        parent2 = self._population[random.randint(0, self._population_size)]
-        shorter_parent = parent1
-        longer_parent = parent2
-        if len(longer_parent) < len(shorter_parent):
-            shorter_parent = parent2
-            longer_parent = parent1
-
-        self._children = []
-        for i in range(7 * self._population_size):
-            child = []
-
-            for j in range(len(shorter_parent)):
-                child.append((shorter_parent[j] + longer_parent[j]) / 2)
-            for j in range(len(shorter_parent), len(longer_parent)):
-                child.append(longer_parent[j])
-
-            self._children.append(child)
-
-    def make_wheel(self, population):
-        wheel = []
-        total = sum(self.fitness(p) for p in population)
-        top = 0
-        for p in population:
-            f = self.fitness(p) / total
-            wheel.append((top, top + f, p))
-            top += f
-        return wheel
-
-    def bin_search(self, wheel, num):
-        mid = len(wheel) // 2
-        low, high, answer = wheel[mid]
-        if low <= num <= high:
-            return answer
-        elif low > num:
-            return self.bin_search(wheel[mid + 1:], num)
-        else:
-            return self.bin_search(wheel[:mid], num)
-
-    def select(self, wheel, n_select):
-        """ this method selects chromosome based on SUS(Stochastic Universal Sampling)"""
-        step_size = 1.0 / n_select
-        new_generation = []
-        r = random.random()
-        new_generation.append(self.bin_search(wheel, r))
-        while len(new_generation) < n_select:
-            r += step_size
-            if r > 1:
-                r %= 1
-            new_generation.append(self.bin_search(wheel, r))
-        return new_generation
-
-    def survivors_selection(self):
-        wheel = self.make_wheel(self._children)
-        self._population = self.select(wheel, self._population_size)
-
-    def train(self, max_iter, data):
-        self._data = data
-
-        self.initialize_population(self._max_range, self._min_range)
-        for i in range(max_iter):
-            self.mutation()
-            self.crossover()
-            self.survivors_selection()
-            print(f'iter {i}')
-
-    def fitness(self, chromosome):
-        return 0
-
-    def calculate_matrices(self, chromosome):
-        g = np.zeros((len(self._data), len(chromosome) // self._gene_fields_number))
-
-        centers = []
-        radius_vectors = []
-        for i in range(len(chromosome)):
-            if i % self._gene_fields_number == 1:
-                center = chromosome[i: i + self._dimension]
-                # radius_vect = [chromosome[i + 2], chromosome[i + 2]]
-                radius_vectors.append(chromosome[i + self._gene_fields_number - 1])
-                centers.append(center)
-
-        for i in range(len(self._data)):
-            for j in range(len(centers)):
-                g[i, j] = math.exp(-1 * (la.norm(self._data[i] - centers[j], 2) / radius_vectors[j]) ** 2)
-
-        self._g = g
-        lam = 0.01
-        self._w = la.inv(g.transpose().dot(g) + lam * np.identity(len(centers))).dot(g.transpose()).dot(self._y_star)
-        self._y = g.dot(self._w)
+# class RBFClassifier:
+#     def __init__(self):
+#         self._data = []
+#         self._dimension = 2  # number of features
+#         self._y_star = []
+#         self._y = []
+#         self._g = []
+#         self._w = []  # weight matrix
+#
+#         self._min_range = -10
+#         self._max_range = 10
+#
+#         self._population = []
+#         self._population_size = 1000
+#         self._chromosome_max_size = 10  # in this version length of chromosomes aren't constant
+#         self._gene_fields_number = 3  # x,y,r
+#         self._tau = 1 / self._gene_fields_number ** 0.5
+#         self._children = []
+#         self._best_chromosome = []
+#
+#     def data(self, d=None):
+#         """getter and setter of data"""
+#         if d:
+#             self._data = d
+#         return self._data
+#
+#     def y(self):
+#         """:returns predicted vector"""
+#         return self._y
+#
+#     def y_star(self):
+#         return self._y_star
+#
+#     def create_random_dataset(self, num_of_data, dimension, cluster_number):
+#         """create random dataset by normal distribution"""
+#         x, y = make_blobs(n_samples=num_of_data, centers=cluster_number, n_features=dimension)
+#         self._dimension = dimension
+#         self._data = x
+#         self._y_star = y
+#
+#         self._max_range = max(self._data[:, 0])
+#         self._min_range = min(self._data[:, 0])
+#
+#     def initialize_population(self, max_range, min_range):
+#         # chromosome representation : <σ,x1,y1,r1,x2,y2,r2,...>
+#         for i in range(self._population_size):
+#             chromosome = [(max_range - min_range) * 0.2]  # add σ to chromosome
+#             for j in range(self._gene_fields_number * random.randint(2, self._chromosome_max_size)):
+#                 chromosome.append(random.random() * (max_range - min_range) + min_range)
+#             self._population.append(chromosome)
+#
+#     def mutation(self):
+#         for chromosome in self._population:
+#             # mutate σ at first
+#             sigma = chromosome[0]
+#             sigma = sigma * math.exp(self._tau * random.normalvariate(mu=0, sigma=1))
+#             chromosome[0] = sigma
+#             # mutate other genes
+#             for i in range(1, len(chromosome)):
+#                 chromosome[i] = chromosome[i] + sigma * random.normalvariate(mu=0, sigma=1)
+#
+#     def crossover(self):
+#         parent1 = self._population[random.randint(0, self._population_size)]
+#         parent2 = self._population[random.randint(0, self._population_size)]
+#         shorter_parent = parent1
+#         longer_parent = parent2
+#         if len(longer_parent) < len(shorter_parent):
+#             shorter_parent = parent2
+#             longer_parent = parent1
+#
+#         self._children = []
+#         for i in range(7 * self._population_size):
+#             child = []
+#
+#             for j in range(len(shorter_parent)):
+#                 child.append((shorter_parent[j] + longer_parent[j]) / 2)
+#             for j in range(len(shorter_parent), len(longer_parent)):
+#                 child.append(longer_parent[j])
+#
+#             self._children.append(child)
+#
+#     def make_wheel(self, population):
+#         wheel = []
+#         total = sum(self.fitness(p) for p in population)
+#         top = 0
+#         for p in population:
+#             f = self.fitness(p) / total
+#             wheel.append((top, top + f, p))
+#             top += f
+#         return wheel
+#
+#     def bin_search(self, wheel, num):
+#         mid = len(wheel) // 2
+#         low, high, answer = wheel[mid]
+#         if low <= num <= high:
+#             return answer
+#         elif low > num:
+#             return self.bin_search(wheel[mid + 1:], num)
+#         else:
+#             return self.bin_search(wheel[:mid], num)
+#
+#     def select(self, wheel, n_select):
+#         """ this method selects chromosome based on SUS(Stochastic Universal Sampling)"""
+#         step_size = 1.0 / n_select
+#         new_generation = []
+#         r = random.random()
+#         new_generation.append(self.bin_search(wheel, r))
+#         while len(new_generation) < n_select:
+#             r += step_size
+#             if r > 1:
+#                 r %= 1
+#             new_generation.append(self.bin_search(wheel, r))
+#         return new_generation
+#
+#     def survivors_selection(self):
+#         wheel = self.make_wheel(self._children)
+#         self._population = self.select(wheel, self._population_size)
+#
+#     def train(self, max_iter, data):
+#         self._data = data
+#
+#         self.initialize_population(self._max_range, self._min_range)
+#         for i in range(max_iter):
+#             self.mutation()
+#             self.crossover()
+#             self.survivors_selection()
+#             print(f'iter {i}')
+#
+#     def fitness(self, chromosome):
+#         return 0
+#
+#     def calculate_matrices(self, chromosome):
+#         g = np.zeros((len(self._data), len(chromosome) // self._gene_fields_number))
+#
+#         centers = []
+#         radius_vectors = []
+#         for i in range(len(chromosome)):
+#             if i % self._gene_fields_number == 1:
+#                 center = chromosome[i: i + self._dimension]
+#                 # radius_vect = [chromosome[i + 2], chromosome[i + 2]]
+#                 radius_vectors.append(chromosome[i + self._gene_fields_number - 1])
+#                 centers.append(center)
+#
+#         for i in range(len(self._data)):
+#             for j in range(len(centers)):
+#                 g[i, j] = math.exp(-1 * (la.norm(self._data[i] - centers[j], 2) / radius_vectors[j]) ** 2)
+#
+#         self._g = g
+#         lam = 0.01
+#         self._w = la.inv(g.transpose().dot(g) + lam * np.identity(len(centers))).dot(g.transpose()).dot(self._y_star)
+#         self._y = g.dot(self._w)
--- run.py
+++ run.py
@@ -8,25 +8,35 @@ import rbf_es
 
 
 def main():
-    sample_num = 100
-    iter_num = 60
-    my_rbf_reg = rbf_es.RBFRegression()
-    my_rbf_reg.create_random_dataset(sample_num, 1)
-    my_rbf_reg.train(iter_num, my_rbf_reg.data())
+    for i in range(4):
+        sample_num = 100
+        iter_num = 30
+        my_rbf_reg = rbf_es.RBFRegression()
+        my_rbf_reg.create_random_dataset(sample_num, 1)
+        my_rbf_reg.train(iter_num, my_rbf_reg.data())
 
-    # -- unsorted
-    # plt.scatter(my_rbf_reg.data(), my_rbf_reg.y(), label='RBF-Net')
-    # plt.scatter(my_rbf_reg.data(), my_rbf_reg.y_star(), '-o', label='true')
-    # -- unsorted
+        plt.figure()
+        # -- unsorted
+        # plt.scatter(my_rbf_reg.data(), my_rbf_reg.y(), label='RBF-Net')
+        # plt.scatter(my_rbf_reg.data(), my_rbf_reg.y_star(), '-o', label='true')
+        # -- unsorted
 
-    # -- sorted samples(usually is in regression)
-    plt.plot(my_rbf_reg.data(), my_rbf_reg.y(), '-o', label='RBF-Net')
-    plt.plot(my_rbf_reg.data(), my_rbf_reg.y_star(), '-o', label='true')
-    plt.tight_layout()
-    # -- sorted
-    plt.legend()
-    plt.title(f'number of samples: {sample_num} ,number of iterations:  {iter_num}')
-    plt.show()
+        # -- sorted samples(usually is in regression)
+        plt.plot(my_rbf_reg.data(), my_rbf_reg.y(), '-o', label='RBF-Net')
+        plt.plot(my_rbf_reg.data(), my_rbf_reg.y_star(), '-o', label='true')
+        plt.tight_layout()
+        # -- sorted
+        plt.legend()
+        plt.title(f'number of samples: {sample_num} ,number of iterations:  {iter_num}, figure index: {i}')
+        # plt.show()
+        plt.savefig(f'{i}-1.png')
+
+        plt.figure()
+        plt.plot(my_rbf_reg._best_fitness_list, '-o', label='best')
+        plt.plot(my_rbf_reg._avg_fitness_list, '-o', label='average')
+        plt.legend()
+        plt.title(f'number of samples: {sample_num} ,number of iterations:  {iter_num}, figure index: {i}')
+        plt.savefig(f'{i}-2.png')
 
 
 if __name__ == '__main__':
